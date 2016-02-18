0be25a97a9ad42373cc84db88330ed1b1d0f3a21
Fajar Bukhaeri <fajarbukhaeri08@gmail.com>
2016-02-18 10:28:29 +0700

    test login

---
+++ login-test/home.php
@@ -0,0 +1,72 @@
+<?php
+
+  require_once("session.php");
+  
+  require_once("class.user.php");
+  $auth_user = new USER();
+  
+  
+  $user_id = $_SESSION['user_session'];
+  
+  $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+  $stmt->execute(array(":user_id"=>$user_id));
+  
+  $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
+  </head><body>
+    <div class="navbar navbar-default navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><span>HBCI Apps</span></a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                Hi' <?php echo $userRow['user_email']; ?>&nbsp;
+                <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="profile.php"><span class="fa fa-user"></span>&nbsp;View Profile</a></li>
+                <li><a href="logout.php?logout=true"><span class="fa fa-sign-out"></span>&nbsp;Sign Out</a></li>
+              </ul>
+            </li>
+            <li class="active">
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Contacts</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="text-center">
+              <b>Selamat Datang <?php print($userRow['user_name']); ?></b>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+</body></html>
---
+++ login-test/index.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+require_once("class.user.php");
+$login = new USER();
+
+if($login->is_loggedin()!="")
+{
+  $login->redirect('home.php');
+}
+
+if(isset($_POST['btn-login']))
+{
+  $uname = strip_tags($_POST['txt_uname_email']);
+  $umail = strip_tags($_POST['txt_uname_email']);
+  $upass = strip_tags($_POST['txt_password']);
+    
+  if($login->doLogin($uname,$umail,$upass))
+  {
+    $login->redirect('home.php');
+  }
+  else
+  {
+    $error = "Wrong Details !";
+  } 
+}
+?>
+
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+  </head><body>
+    <div class="cover">
+      <div class="cover-image"></div>
+    </div>
+    <div class="section section-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <form role="form" class="text-justify" method="post">
+              <div id="error">
+                <?php if(isset($error)) { ?>
+                <div class="alert alert-danger">
+                  <button contenteditable="false" type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                  <i class="fa fa-warning"></i>&nbsp;
+                  <?php echo $error; ?>!</div>
+                <?php } ?>
+              </div>
+              <div class="form-group">
+                <input class="form-control input-sm" placeholder="Username or Email" type="text" name="txt_uname_email">
+                <input class="form-control input-sm" placeholder="Password" type="password" name="txt_password">
+              </div>
+              <button type="submit" class="btn btn-block btn-primary" name="btn-login">
+                <i class="fa fa-fw fa-sign-in"></i>&nbsp;Sign in</button>
+            </form>
+            <div>
+                <a href="#">Forgot Password ?</a>
+                <a class="sign-up" href="sign-up.php">Sign Up</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+</body></html>
